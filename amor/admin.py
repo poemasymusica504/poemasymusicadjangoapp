4b5ac1585fb6a6d0e58229a30eb011db25a9ce42
davis45368 <poemas.y.musica504@gmail.com>
2023-07-04 19:22:50 -0500

    cambio admin Poema

--- amor/admin.py
+++ amor/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Poema
 # Register your models here.
 
-admin.site.register(Poema)
+@admin.register(Poema)
+
+class ValorAdmin(admin.ModelAdmin):
+    list_display = ('id','escritor','titulo','fecha')
+    search_fields = ('id','escritor','titulo')
